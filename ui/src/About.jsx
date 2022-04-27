36c8706e7290efb5d662899172ba8562ab0fb2b2
Nishu Singh <nsingh2878@sdsu.edu>
2022-04-27 00:08:56 -0700

    Implementation of Assignment 7

---
+++ ui/src/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function About() {
+  return (
+    <div className="text-center">
+      <h3>My Company Inventory</h3>
+      <h4>
+        Full Stack (Mern Stack) Assignment for Company Inventory
+      </h4>
+    </div>
+  );
+}
